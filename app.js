7547d912f103d1edf00047e19ea8770cfb716360
Chad Altenberger <chadaltenberger@gmail.com>
2021-04-10 18:32:32 -0400

    ACreated a filtering option and added functionality and styling.

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoBtn = document.querySelector('.todo-btn');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 //EVENT HANDLERS
 todoBtn.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteOrCheck);
+filterOption.addEventListener('change', filterTodos);
 
 //FUNCTIONS
 function addTodo(e) {
@@ -14,7 +16,7 @@ function addTodo(e) {
 	//CREATE TODO DIV
 	const todoDiv = document.createElement('div');
 	todoDiv.classList.add('todo');
-	//CREATE LI
+	//CREATE <li>
 	const newTodo = document.createElement('li');
 	newTodo.innerText = todoInput.value;
 	newTodo.classList.add('todo-item');
@@ -51,4 +53,29 @@ function deleteOrCheck(e) {
 		const todo = item.parentElement;
 		todo.classList.toggle('completed'); //adds/removes the class 'completed' to the todoList div
 	}
+	filterTodos();
+}
+
+function filterTodos(e) {
+	const todos = todoList.childNodes; //gets access to each list item (todo)
+	todos.forEach(function (todo) {
+		switch (filterOption.value) {
+			case 'all':
+				todo.style.display = 'flex';
+				break;
+			case 'completed':
+				if (todo.classList.contains('completed')) {
+					todo.style.display = 'flex';
+				} else {
+					todo.style.display = 'none';
+				}
+				break;
+			case 'uncompleted':
+				if (!todo.classList.contains('completed')) {
+					todo.style.display = 'flex';
+				} else {
+					todo.style.display = 'none';
+				}
+		}
+	});
 }
